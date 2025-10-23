a90ca783e101a7677e4dab3b9d4e692d9a4e594a
Keffonjoro <92052505+Keffonjoro@users.noreply.github.com>
2025-10-23 23:14:29 +0300

    Rename project and add configuration class
    Updated configuration class with new settings.

--- church_portal/config.py
+++ church_portal/config.py
@@ -1,1 +1,5 @@
-# Samburu_Tycoon
+# TKN
+class Config:
+    SECRET_KEY = 'your_secret_key_here'  # Change this to a random string
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/church.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

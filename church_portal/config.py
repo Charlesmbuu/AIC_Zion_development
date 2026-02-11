161fb897e768138fbe2e488417d2549780d2ccfe
Charles Mburu <95209529+Charlesmbuu@users.noreply.github.com>
2026-02-11 17:13:56 +0300

    Delete church_portal directory

--- church_portal/config.py
+++
@@ -1,5 +0,0 @@
-# TKN
-class Config:
-    SECRET_KEY = 'your_secret_key_here'  # Change this to a random string
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/church.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False

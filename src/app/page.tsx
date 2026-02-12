2871bb612790618e1cfd7b3250d49979df0b6452
Charles Mburu <95209529+Charlesmbuu@users.noreply.github.com>
2026-02-12 11:08:25 +0300

    Add components to HomePage in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,17 @@
+import Hero from "@/components/sections/Hero";
+import ServiceTimes from "@/components/sections/ServiceTimes";
+import SermonPreview from "@/components/sections/SermonPreview";
+import EventPreview from "@/components/sections/EventPreview";
+import CallToAction from "@/components/sections/CallToAction";
 
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <ServiceTimes />
+      <SermonPreview />
+      <EventPreview />
+      <CallToAction />
+    </>
+  );
+}

e3b95dc710f81e5dbbad179dd5555a32afbea976
Charles Mburu <95209529+Charlesmbuu@users.noreply.github.com>
2026-02-12 11:11:03 +0300

    Add Loading component for loading state

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,1 +1,7 @@
-
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="animate-pulse">Loading...</p>
+    </div>
+  );
+}
